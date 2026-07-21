13a8486a3884219f22ae6a22c826980b44471a78
Jay Wedgeworth <12656028+jaywedgeworth22@users.noreply.github.com>
2026-07-20 22:28:56 -0500

    fix(deno): use deno.land/x/zod and module: preserve to fix inference bugs with npm:zod

--- app/shared.ts
+++ app/shared.ts
@@ -1,2 +1,2 @@
-// @deno-types="./vendor/congress-trading-shared/dist/index.d.ts"
+// @deno-types="./vendor/congress-trading-shared/src/index.ts"
 export * from "./vendor/congress-trading-shared/dist/index.mjs";
