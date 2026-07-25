43f30ef02707750b4003e5431dc955dbdf63c9b8
Jay Wedgeworth <12656028+jaywedgeworth22@users.noreply.github.com>
2026-07-24 21:55:20 -0500

    Mirror effort log (#945)

--- clients/pwa/functions/api/[[path]].ts
+++
@@ -1,7 +0,0 @@
-const WORKER_ORIGIN = 'https://congress.trade';
-
-export function onRequest({ request }: { request: Request }): Promise<Response> {
-  const incoming = new URL(request.url);
-  const target = new URL(`${incoming.pathname}${incoming.search}`, WORKER_ORIGIN);
-  return fetch(new Request(target, request));
-}
